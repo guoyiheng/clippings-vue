4bc2d8d41ff1bbf56a1bbec5358201b62519c9b1
gyh <mypridelife@163.com>
2020-11-05 16:42:55 +0800

    fix: pwd

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-cd /var/lib/jenkins/workspace/clippings-vue #进入Jenkins工作空间下hxkj项目目录
 node -v #检测node版本（此条命令非必要）
 npm -v #检测npm版本（此条命令非必要）
 npm config set registry https://registry.npm.taobao.org #把npm源设置为淘宝源（这个你懂的）
@@ -12,6 +11,6 @@ npm run build #打包
 APP_NAME="kindle.gyh.cool"
 dstr=`date +%Y-%m-%d-%H-%M-%S`
 
-mv /var/lib/jenkins/workspace/${APP_NAME}/dist /usr/local/openresty/nginx/html/${APP_NAME}-${dstr}
+mv /dist /usr/local/openresty/nginx/html/${APP_NAME}-${dstr}
 rm -rf /usr/local/openresty/nginx/html/${APP_NAME}
 ln -s /usr/local/openresty/nginx/html/${APP_NAME}-${dstr} /usr/local/openresty/nginx/html/${APP_NAME}
